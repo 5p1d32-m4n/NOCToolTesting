6ce3bbb8c74b3d81034ee226e82e8b80f47cdf12
Brandon J. Baker Escarraman <brandon.j.baker1011@gmail.com>
2021-07-24 19:09:37 -0400

    working on list checkbox functionality.

--- frontend/src/views/InitialForm.vue
+++ frontend/src/views/InitialForm.vue
@@ -41,7 +41,7 @@
               id="dropdownMenuButton1"
               data-bs-toggle="dropdown"
               aria-expanded="false"
-              style="width:200px;"
+              style="width: 200px"
             >
               Servicios Impactados
             </button>
@@ -59,7 +59,7 @@
               id="dropdownMenuButton1"
               data-bs-toggle="dropdown"
               aria-expanded="false"
-              style="width:200px;"
+              style="width: 200px"
             >
               Tipo de Averia
             </button>
@@ -77,7 +77,7 @@
               id="dropdownMenuButton1"
               data-bs-toggle="dropdown"
               aria-expanded="false"
-              style="width:200px;"
+              style="width: 200px"
             >
               Clientes Impactados
             </button>
@@ -95,7 +95,7 @@
               id="dropdownMenuButton1"
               data-bs-toggle="dropdown"
               aria-expanded="false"
-              style="width:200px;"
+              style="width: 200px"
             >
               Servicios Impactados
             </button>
@@ -113,7 +113,7 @@
               id="dropdownMenuButton1"
               data-bs-toggle="dropdown"
               aria-expanded="false"
-              style="width:200px;"
+              style="width: 200px"
             >
               Servicios Impactados
             </button>
@@ -129,99 +129,31 @@
       <!-- AM4 Map chart -->
       <div class="columns">
         <div class="column">
-          <div id="mapchart"></div>
+          <div id="mapchart" @click="appendToMunicipalitiesList"></div>
         </div>
         <!-- List of Municipality Checkboxesw -->
         <div class="column">
           <div id="municipality-talbe">
             <table class="table is-bordered is-fullwidth overflow-scroll">
-            <thead>
-              <th>Municipios Impactados</th>
-            </thead>
-            <tbody>
-              <tr><td><div class="checkbox"><label><input type="checkbox" name="map" value="Adjuntas"/>Adjuntas</label></div></td></tr>
-              <tr><td><div class="checkbox"><label><input type="checkbox" name="map" value="Aguada"/>Aguada</label></div></td></tr>
-              <tr><td><div class="checkbox"><label><input type="checkbox" name="map" value="Aguadilla"/>Aguadilla</label></div></td></tr>
-              <tr><td><div class="checkbox"><label><input type="checkbox" name="map" value="Aguas Buenas"/>Aguas Buenas</label></div></td></tr>
-              <tr><td><div class="checkbox"><label><input type="checkbox" name="map" value="Aibonito"/>Aibonito</label></div></td></tr>
-              <tr><td><div class="checkbox"><label><input type="checkbox" name="map" value="Añasco"/>Añasco</label></div></td></tr>
-              <tr><td><div class="checkbox"><label><input type="checkbox" name="map" value="Arecibo"/>Arecibo</label></div></td></tr>
-              <tr><td><div class="checkbox"><label><input type="checkbox" name="map" value="Arroyo"/>Arroyo</label></div></td></tr>
-              <tr><td><div class="checkbox"><label><input type="checkbox" name="map" value="Barceloneta"/>Barceloneta</label></div></td></tr>
-              <tr><td><div class="checkbox"><label><input type="checkbox" name="map" value="Barranquitas"/>Barranquitas</label></div></td></tr>
-              <tr><td><div class="checkbox"><label><input type="checkbox" name="map" value="Bayamón"/>Bayamón</label></div></td></tr>
-              <tr><td><div class="checkbox"><label><input type="checkbox" name="map" value="Cabo Rojo"/>Cabo Rojo</label></div></td></tr>
-              <tr><td><div class="checkbox"><label><input type="checkbox" name="map" value="Caguas"/>Caguas</label></div></td></tr>
-              <tr><td><div class="checkbox"><label><input type="checkbox" name="map" value="Camuy"/>Camuy</label></div></td></tr>
-              <tr><td><div class="checkbox"><label><input type="checkbox" name="map" value="Canóvanas"/>Canóvanas</label></div></td></tr>
-              <tr><td><div class="checkbox"><label><input type="checkbox" name="map" value="Carolina"/>Carolina</label></div></td></tr>
-              <tr><td><div class="checkbox"><label><input type="checkbox" name="map" value="Cataño"/>Cataño</label></div></td></tr>
-              <tr><td><div class="checkbox"><label><input type="checkbox" name="map" value="Cayey"/>Cayey</label></div></td></tr>
-              <tr><td><div class="checkbox"><label><input type="checkbox" name="map" value="Ceiba"/>Ceiba</label></div></td></tr>
-              <tr><td><div class="checkbox"><label><input type="checkbox" name="map" value="Ciales"/>Ciales</label></div></td></tr>
-              <tr><td><div class="checkbox"><label><input type="checkbox" name="map" value="Cidra"/>Cidra</label></div></td></tr>
-              <tr><td><div class="checkbox"><label><input type="checkbox" name="map" value="Coamo"/>Coamo</label></div></td></tr>
-              <tr><td><div class="checkbox"><label><input type="checkbox" name="map" value="Comerío"/>Comerío</label></div></td></tr>
-              <tr><td><div class="checkbox"><label><input type="checkbox" name="map" value="Corozal"/>Corozal</label></div></td></tr>
-              <tr><td><div class="checkbox"><label><input type="checkbox" name="map" value="Culebra"/>Culebra</label></div></td></tr>
-              <tr><td><div class="checkbox"><label><input type="checkbox" name="map" value="Dorado"/>Dorado</label></div></td></tr>
-              <tr><td><div class="checkbox"><label><input type="checkbox" name="map" value="Fajardo"/>Fajardo</label></div></td></tr>
-              <tr><td><div class="checkbox"><label><input type="checkbox" name="map" value="Florida"/>Florida</label></div></td></tr>
-              <tr><td><div class="checkbox"><label><input type="checkbox" name="map" value="Guánica"/>Guánica</label></div></td></tr>
-              <tr><td><div class="checkbox"><label><input type="checkbox" name="map" value="Guayama"/>Guayama</label></div></td></tr>
-              <tr><td><div class="checkbox"><label><input type="checkbox" name="map" value="Guayanilla"/>Guayanilla</label></div></td></tr>
-              <tr><td><div class="checkbox"><label><input type="checkbox" name="map" value="Guaynabo"/>Guaynabo</label></div></td></tr>
-              <tr><td><div class="checkbox"><label><input type="checkbox" name="map" value="Gurabo"/>Gurabo</label></div></td></tr>
-              <tr><td><div class="checkbox"><label><input type="checkbox" name="map" value="Hatillo"/>Hatillo</label></div></td></tr>
-              <tr><td><div class="checkbox"><label><input type="checkbox" name="map" value="Hormigueros"/>Hormigueros</label></div></td></tr>
-              <tr><td><div class="checkbox"><label><input type="checkbox" name="map" value="Humacao"/>Humacao</label></div></td></tr>
-              <tr><td><div class="checkbox"><label><input type="checkbox" name="map" value="Isabela"/>Isabela</label></div></td></tr>
-              <tr><td><div class="checkbox"><label><input type="checkbox" name="map" value="Jayuya"/>Jayuya</label></div></td></tr>
-              <tr><td><div class="checkbox"><label><input type="checkbox" name="map" value="JuanaDíaz"/>JuanaDíaz</label></div></td></tr>
-              <tr><td><div class="checkbox"><label><input type="checkbox" name="map" value="Juncos"/>Juncos</label></div></td></tr>
-              <tr><td><div class="checkbox"><label><input type="checkbox" name="map" value="Lajas"/>Lajas</label></div></td></tr>
-              <tr><td><div class="checkbox"><label><input type="checkbox" name="map" value="Lares"/>Lares</label></div></td></tr>
-              <tr><td><div class="checkbox"><label><input type="checkbox" name="map" value="Las Marías"/>Las Marías</label></div></td></tr>
-              <tr><td><div class="checkbox"><label><input type="checkbox" name="map" value="Las Piedras"/>Las Piedras</label></div></td></tr>
-              <tr><td><div class="checkbox"><label><input type="checkbox" name="map" value="Loíza"/>Loíza</label></div></td></tr>
-              <tr><td><div class="checkbox"><label><input type="checkbox" name="map" value="Luquillo"/>Luquillo</label></div></td></tr>
-              <tr><td><div class="checkbox"><label><input type="checkbox" name="map" value="Manatí"/>Manatí</label></div></td></tr>
-              <tr><td><div class="checkbox"><label><input type="checkbox" name="map" value="Maricao"/>Maricao</label></div></td></tr>
-              <tr><td><div class="checkbox"><label><input type="checkbox" name="map" value="Maunabo"/>Maunabo</label></div></td></tr>
-              <tr><td><div class="checkbox"><label><input type="checkbox" name="map" value="Mayagüez"/>Mayagüez</label></div></td></tr>
-              <tr><td><div class="checkbox"><label><input type="checkbox" name="map" value="Moca"/>Moca</label></div></td></tr>
-              <tr><td><div class="checkbox"><label><input type="checkbox" name="map" value="Morovis"/>Morovis</label></div></td></tr>
-              <tr><td><div class="checkbox"><label><input type="checkbox" name="map" value="Naguabo"/>Naguabo</label></div></td></tr>
-              <tr><td><div class="checkbox"><label><input type="checkbox" name="map" value="Naranjito"/>Naranjito</label></div></td></tr>
-              <tr><td><div class="checkbox"><label><input type="checkbox" name="map" value="Orocovis"/>Orocovis</label></div></td></tr>
-              <tr><td><div class="checkbox"><label><input type="checkbox" name="map" value="Patillas"/>Patillas</label></div></td></tr>
-              <tr><td><div class="checkbox"><label><input type="checkbox" name="map" value="Peñuelas"/>Peñuelas</label></div></td></tr>
-              <tr><td><div class="checkbox"><label><input type="checkbox" name="map" value="Ponce"/>Ponce</label></div></td></tr>
-              <tr><td><div class="checkbox"><label><input type="checkbox" name="map" value="Quebradillas"/>Quebradillas</label></div></td></tr>
-              <tr><td><div class="checkbox"><label><input type="checkbox" name="map" value="Rincón"/>Rincón</label></div></td></tr>
-              <tr><td><div class="checkbox"><label><input type="checkbox" name="map" value="RíoGrande"/>RíoGrande</label></div></td></tr>
-              <tr><td><div class="checkbox"><label><input type="checkbox" name="map" value="Sabana Grande"/>Sabana Grande</label></div></td></tr>
-              <tr><td><div class="checkbox"><label><input type="checkbox" name="map" value="Salinas"/>Salinas</label></div></td></tr>
-              <tr><td><div class="checkbox"><label><input type="checkbox" name="map" value="San Germán"/>San Germán</label></div></td></tr>
-              <tr><td><div class="checkbox"><label><input type="checkbox" name="map" value="San Juan"/>San Juan</label></div></td></tr>
-              <tr><td><div class="checkbox"><label><input type="checkbox" name="map" value="capital"/>capital</label></div></td></tr>
-              <tr><td><div class="checkbox"><label><input type="checkbox" name="map" value="San Lorenzo"/>San Lorenzo</label></div></td></tr>
-              <tr><td><div class="checkbox"><label><input type="checkbox" name="map" value="San Sebastián"/>San Sebastián</label></div></td></tr>
-              <tr><td><div class="checkbox"><label><input type="checkbox" name="map" value="Santa Isabel"/>Santa Isabel</label></div></td></tr>
-              <tr><td><div class="checkbox"><label><input type="checkbox" name="map" value="Toa Alta"/>Toa Alta</label></div></td></tr>
-              <tr><td><div class="checkbox"><label><input type="checkbox" name="map" value="Toa Baja"/>Toa Baja</label></div></td></tr>
-              <tr><td><div class="checkbox"><label><input type="checkbox" name="map" value="Trujillo Alto"/>Trujillo Alto</label></div></td></tr>
-              <tr><td><div class="checkbox"><label><input type="checkbox" name="map" value="Utuado"/>Utuado</label></div></td></tr>
-              <tr><td><div class="checkbox"><label><input type="checkbox" name="map" value="Vega Alta"/>Vega Alta</label></div></td></tr>
-              <tr><td><div class="checkbox"><label><input type="checkbox" name="map" value="Vega Baja"/>Vega Baja</label></div></td></tr>
-              <tr><td><div class="checkbox"><label><input type="checkbox" name="map" value="Vieques"/>Vieques</label></div></td></tr>
-              <tr><td><div class="checkbox"><label><input type="checkbox" name="map" value="Villalba"/>Villalba</label></div></td></tr>
-              <tr><td><div class="checkbox"><label><input type="checkbox" name="map" value="Yabucoa"/>Yabucoa</label></div></td></tr>
-              <tr><td><div class="checkbox"><label><input type="checkbox" name="map" value="Yauco"/>Yauco</label></div></td></tr>
-            </tbody>
-          </table>
+              <thead>
+                <th>Municipios Impactados</th>
+              </thead>
+              
+              <tbody>
+                <tr v-for="(country) in listOfMunicipalities" v-bind:key="country">
+                  <div class="checkbox">
+                      <label
+                        ><input
+                          type="checkbox"
+                          name="map"
+                          value="country"
+                        />{{country}}</label
+                      >
+                    </div>
+                </tr>
+              </tbody>
+            </table>
           </div>
-          
         </div>
       </div>
     </form>
@@ -248,9 +180,20 @@ export default {
   data() {
     return {
       report: {},
+      listOfMunicipalities: [],
+      selected: this.listOfMunicipalities
     };
   },
+  watch:{
+    listOfMunicipalities:{
+      immediate: true,
+      handler(){
+        this.changeStateOfCountry()
+      }
+    },
+  },
   methods: {
+    // TODO: Make sure that this has the parts it needs to process report objects properly
     postReport(report) {
       axios
         .post(`/api/report-create/`, report)
@@ -261,7 +204,23 @@ export default {
           console.log(error);
         });
     },
+    changeStateOfCountry(countries) {
+      this.listOfMunicipalities = countries;
+    },
+    appendToMunicipalitiesList: function(event){
+      event.target.isActive = !event.target.isActive;
+      let municipality = event.target.dataItem.dataContext.name;
+      if ( event.target.isActive === true){
+        this.listOfMunicipalities.push(municipality);
+        console.log(this.listOfMunicipalities);
+      }else{
+        this.listOfMunicipalities.pop(municipality);
+        console.log(this.listOfMunicipalities);
+      }
+    }
+
   },
+
   mounted() {
     // Setting the Doc Tittle
     document.title = "Formulario de Reporte Inicial";
@@ -284,7 +243,32 @@ export default {
 
     // Zooom Controls for the map
     map.seriesContainer.wheelable = false;
+    map.seriesContainer.draggable = false;
     map.maxZoomLevel = 1;
+
+    // configuration for active state in map.
+    let activeState = polygonTemplate.states.create("active");
+    activeState.properties.fill = am4core.color("#FF0000");
+
+    // event to switch active state.
+    polygonTemplate.events.on("hit", this.appendToMunicipalitiesList);
+
+    // console.log(polygonSeries.datal)
+    
+    let polyData = polygonSeries.data
+    console.log(polyData)
+    
+
+    // let listFields = polygonSeries.dataFields
+    // let arrayPR = []
+    // for (let entry = 0; entry <  listOfPR.length; entry ++){
+    //   // arrayPR.push(listOfPR[entry].name)
+    //   console.log(listOfPR[entry].value.name)
+    //   console.log("in the loop")
+    // }
+    // console.log(arrayPR)
+    // console.log(listOfPR)
+    // console.log(listFields)
   },
 };
 
@@ -296,7 +280,7 @@ export default {
   height: 400px;
 }
 
-#municipality-talbe{
+#municipality-talbe {
   height: 400px;
   overflow: auto;
   text-align: justify;
