a3ddbb592d589f074601749669166ce61eba299b
Brandon J. Baker <brandon.j.baker1011@gmail.com>
2021-08-23 18:55:03 -0400

    Added Bootstrap and currently migrating Vue 3 components to Vue 2.

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,11 +2,14 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-
-Vue.config.productionTip = false;
-
+import axios from "axios";
+import BootstrapVue from 'bootstrap-vue/dist/bootstrap-vue.esm';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+import 'bootstrap/dist/css/bootstrap.css';
 new Vue({
   router,
   store,
+  axios,
+  BootstrapVue,
   render: (h) => h(App),
 }).$mount("#app");
