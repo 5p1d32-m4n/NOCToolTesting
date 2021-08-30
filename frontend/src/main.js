8fa6f713e4683a6c9f6e55d3495f6fcd6c3e469a
Brandon J. Baker <brandon.j.baker1011@gmail.com>
2021-08-29 20:50:51 -0400

    Moved imports and used Vue instances for bootstrap-vue.

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,13 +3,19 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import axios from "axios";
-import BootstrapVue from 'bootstrap-vue/dist/bootstrap-vue.esm';
-import 'bootstrap-vue/dist/bootstrap-vue.css';
+import {
+  BootstrapVue,
+  IconsPlugin
+} from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+
+Vue.use(axios)
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
+
 new Vue({
   router,
   store,
-  axios,
-  BootstrapVue,
   render: (h) => h(App),
 }).$mount("#app");
