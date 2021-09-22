234b3c27be1610db96f2444fd134e2ca976cbf2c
Brandon J. Baker <brandon.j.baker1011@gmail.com>
2021-09-21 21:53:49 -0400

    Changed Service and client fields from foreign keys to json fields.

--- report_builder/models.py
+++ report_builder/models.py
@@ -37,7 +37,7 @@ class Report(models.Model):
         ("Actualizacion", "Actualizacion"),
         ("Final", "Final")
     )
-
+    # Unique per report fields
     report_type = models.CharField(max_length=15, choices=STATES, default=None)
     noc_ticket = models.CharField(
         max_length=15, primary_key=True, unique=True, default=None)
@@ -49,18 +49,24 @@ class Report(models.Model):
         auto_now=False, auto_now_add=False, blank=True, editable=True)
     notes = models.TextField(blank=True, null=True, max_length=500)
     municipalities = models.CharField(max_length=200)
-    services = models.CharField(blank=True, default=None, max_length=250,
-                                null=True)
-    service_amount = models.CharField(blank=True, default="0", max_length=250,
-                                      null=True)
-    clients = models.CharField(blank=True, default=None, max_length=250,
-                               null=True)
-    client_amount = models.CharField(blank=True, default="0", max_length=250,
-                                     null=True)
+
+    # Repeatedly used fields.
+    # TODO: Deprecate these fields.
+    # services = models.CharField(blank=True, default=None, max_length=250,
+    #                             null=True)
+    # service_amount = models.CharField(blank=True, default="0",max_length=250,
+    #                                   null=True)
+    # clients = models.CharField(blank=True, default=None, max_length=250,
+    #                            null=True)
+    # client_amount = models.CharField(blank=True, default="0", max_length=250,
+    #                                  null=True)
     outage_type = models.CharField(blank=True, default=None, max_length=250,
                                    null=True)
     causes = models.CharField(blank=True, default=None, max_length=250,
                               null=True)
+    # Testing Json fileds
+    services = models.JSONField(default=dict)
+    clients = models.JSONField(default=dict)
 
     def __str__(self):
         return f'Evento: {self.noc_ticket}, en la fecha {self.date_of_outage}'
