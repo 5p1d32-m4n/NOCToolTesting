c4a28d8f5e130d7db584d68039b9650785e48ac0
Brandon J. Baker <brandon.j.baker1011@gmail.com>
2021-10-07 23:00:30 -0400

    local reconfiguration

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -12,8 +12,9 @@ import 'bootstrap-vue/dist/bootstrap-vue.css';
 
 Vue.use(BootstrapVue, IconsPlugin)
 // Vue.use(IconsPlugin)
+// Local URL for Django API http://127.0.0.1:8000
 //  Insert here custom url for live hosting url to fetch with axios: https://8000-lime-cattle-yegqyfpg.ws-us18.gitpod.io
-axios.defaults.baseURL = 'https://8000-lime-cattle-yegqyfpg.ws-us18.gitpod.io'
+axios.defaults.baseURL = 'http://127.0.0.1:8000'
 new Vue({
   router,
   store,
